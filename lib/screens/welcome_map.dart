d058b6985b1e5663b2d4dadce3d9a9f690a23064
StephaneBlackPanther <stephane@smartcodegroup.com>
2021-01-15 12:38:12 +0100

    skeleton

--- lib/screens/welcome_map.dart
+++ lib/screens/welcome_map.dart
@@ -4,6 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:mapane/routes.dart';
 import '../utils/theme_mapane.dart';
 import 'package:mapane/utils/size_config.dart';
+import 'package:flutter_skeleton/flutter_skeleton.dart';
 
 class WelcomeMap extends StatefulWidget {
   @override
@@ -43,18 +44,19 @@ class _MyAppState extends State<WelcomeMap> {
               children: [
                 Padding(
                   padding: EdgeInsets.symmetric(
-                      vertical: getSize(9, "heigth", context),
+                      vertical: getSize(9, "height", context),
                       horizontal: getSize(36, "width", context)),
                   child: Image.asset(
                     'assets/images/Logo-long-edited.png',
-                    height: getSize(39, "heigth", context),
+                    height: getSize(39, "height", context),
                   ),
                 ),
                 SizedBox(
-                  height: getSize(34, "heigth", context),
+                  height: getSize(34, "height", context),
                 ),
                 Padding(
-                  padding: EdgeInsets.symmetric(vertical: 0, horizontal: getSize(26,"width",context)),
+                  padding: EdgeInsets.symmetric(
+                      vertical: 0, horizontal: getSize(26, "width", context)),
                   child: Container(
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -64,13 +66,15 @@ class _MyAppState extends State<WelcomeMap> {
                           children: [
                             Text(
                               "Mon historique",
-                              style:
-                                  Theme.of(context).textTheme.headline1.copyWith(
-                                        fontSize: getSize(24, "heigth", context),
-                                        fontWeight: FontWeight.w400,
-                                        color: AppColors.blackColor,
-                                        letterSpacing: 0,
-                                      ),
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline1
+                                  .copyWith(
+                                    fontSize: getSize(24, "height", context),
+                                    fontWeight: FontWeight.w400,
+                                    color: AppColors.blackColor,
+                                    letterSpacing: 0,
+                                  ),
                             )
                           ],
                         ),
@@ -78,7 +82,7 @@ class _MyAppState extends State<WelcomeMap> {
                           child: Center(
                             child: Image.asset(
                               'assets/images/refresh-icon.png',
-                              height: getSize(20, "heigth", context),
+                              height: getSize(20, "height", context),
                             ),
                           ),
                         )
@@ -87,10 +91,11 @@ class _MyAppState extends State<WelcomeMap> {
                   ),
                 ),
                 SizedBox(
-                  height: getSize(14, "heigth", context),
+                  height: getSize(14, "height", context),
                 ),
                 Padding(
-                  padding: EdgeInsets.symmetric(vertical: 0, horizontal: getSize(26,"width",context)),
+                  padding: EdgeInsets.symmetric(
+                      vertical: 0, horizontal: getSize(6, "width", context)),
                   child: DefaultTabController(
                     length: 4,
                     child: Column(
@@ -98,16 +103,21 @@ class _MyAppState extends State<WelcomeMap> {
                       children: <Widget>[
                         Container(
                           child: TabBar(
-                              indicatorSize: TabBarIndicatorSize.tab,
-                              indicatorPadding: EdgeInsets.symmetric(
-                                  vertical: 0, horizontal: 0),
+                              indicatorSize: TabBarIndicatorSize.label,
+                              indicatorPadding: EdgeInsets.zero,
                               labelPadding: EdgeInsets.symmetric(
                                   vertical: 0, horizontal: 0),
                               labelStyle: Theme.of(context)
                                   .textTheme
                                   .bodyText1
                                   .copyWith(
-                                      fontSize: getSize(12, "heigth", context)),
+                                      fontSize: getSize(12, "height", context)),
+                              unselectedLabelStyle: Theme.of(context)
+                                  .textTheme
+                                  .bodyText1
+                                  .copyWith(
+                                      color: Colors.black,
+                                      fontSize: getSize(12, "height", context)),
                               labelColor: Color(0xFF25296A),
                               indicatorColor: Color(0xFF25296A),
                               indicator: BoxDecoration(
@@ -115,51 +125,92 @@ class _MyAppState extends State<WelcomeMap> {
                                 borderRadius: BorderRadius.circular(5),
                               ),
                               tabs: [
-                                Tab(
-                                  text: "Tout",
+                                Container(
+                                  width: getSize(56, "width", context),
+                                  height: getSize(27, "height", context),
+                                  child: Tab(
+                                    text: "Tout",
+                                  ),
                                 ),
-                                Tab(text: "Embouteillages"),
-                                Tab(text: "Route barrée"),
-                                Tab(text: "Autres"),
+                                Container(
+                                  width: getSize(100, "width", context),
+                                  height: getSize(27, "height", context),
+                                  child: Tab(
+                                    text: "Embouteillages",
+                                  ),
+                                ),
+                                Container(
+                                  height: getSize(27, "height", context),
+                                  child: Tab(
+                                    text: "Route barrée",
+                                  ),
+                                ),
+                                Container(
+                                  width: getSize(56, "width", context),
+                                  height: getSize(27, "height", context),
+                                  child: Tab(
+                                    text: "Autres",
+                                  ),
+                                ),
                               ]),
                         ),
                         Container(
                           //Add this to give height
                           height: MediaQuery.of(context).size.height / 3,
-                          child: TabBarView(children: [
-                            Container(
-                              child: Padding(
-                                padding: EdgeInsets.symmetric(
-                                    vertical: getSize(46, "heigth", context),
-                                    horizontal: 0),
-                                child: Text("Home Body"),
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 0,
+                                horizontal: getSize(26, "width", context)),
+                            child: TabBarView(children: [
+                              Container(
+                                child: Padding(
+                                  padding: EdgeInsets.symmetric(
+                                      vertical: getSize(46, "height", context),
+                                      horizontal: 0),
+                                  child: ListSkeleton(
+                                    style: SkeletonStyle(
+                                      backgroundColor: Colors.transparent,
+                                      padding: EdgeInsets.symmetric(
+                                  vertical: 0, horizontal: 0),
+                                      theme: SkeletonTheme.Light,
+                                      isShowAvatar: false,
+                                      barCount: 2,
+                                      colors: [
+                                        Color(0x26A3A3A3),
+                                        Color(0x4007116E),
+                                        Color(0x4007116E)
+                                      ],
+                                      isAnimation: true,
+                                    ),
+                                  ),
+                                ),
                               ),
-                            ),
-                            Container(
-                              child: Padding(
-                                padding: EdgeInsets.symmetric(
-                                    vertical: getSize(46, "heigth", context),
-                                    horizontal: 0),
-                                child: Text("Home Body 2"),
+                              Container(
+                                child: Padding(
+                                  padding: EdgeInsets.symmetric(
+                                      vertical: getSize(46, "height", context),
+                                      horizontal: 0),
+                                  child: Text("Home Body 2"),
+                                ),
                               ),
-                            ),
-                            Container(
-                              child: Padding(
-                                padding: EdgeInsets.symmetric(
-                                    vertical: getSize(46, "heigth", context),
-                                    horizontal: 0),
-                                child: Text("Home Body 3"),
+                              Container(
+                                child: Padding(
+                                  padding: EdgeInsets.symmetric(
+                                      vertical: getSize(46, "height", context),
+                                      horizontal: 0),
+                                  child: Text("Home Body 3"),
+                                ),
                               ),
-                            ),
-                            Container(
-                              child: Padding(
-                                padding: EdgeInsets.symmetric(
-                                    vertical: getSize(46, "heigth", context),
-                                    horizontal: 0),
-                                child: Text("Home Body 4"),
+                              Container(
+                                child: Padding(
+                                  padding: EdgeInsets.symmetric(
+                                      vertical: getSize(46, "height", context),
+                                      horizontal: 0),
+                                  child: Text("Home Body 4"),
+                                ),
                               ),
-                            ),
-                          ]),
+                            ]),
+                          ),
                         ),
                       ],
                     ),
