9a9c7f21dc0b0b6ce31e466e9f50a73941b2cdb3
SherlockHolmes2045 <lemovou@gmail.com>
2021-01-20 10:33:34 +0100

    added connectivity interceptor

--- lib/networking/retry_interceptor.dart
+++ lib/networking/retry_interceptor.dart
@@ -1,16 +1,27 @@
+import 'dart:io';
+import 'package:connectivity/connectivity.dart';
 import 'package:dio/dio.dart';
+import 'package:mapane/networking/dio_connectivity_reques_retrier.dart';
 
 class RetryOnAuthFailInterceptor extends Interceptor{
+  final DioConnectivityRequestRetrier requestRetrier;
+
+  RetryOnAuthFailInterceptor(this.requestRetrier);
 
   @override
   Future onError(DioError err) async {
     if(_shouldRefresh(err)){
-      // TODO: implement onError
-
+      try {
+        return requestRetrier.scheduleRequestRetry(err.request);
+      }catch(e){
+        return e;
+      }
     }
     return err;
   }
   bool _shouldRefresh(DioError err){
-    return err.response.statusCode == 401;
+    return err.type  == DioErrorType.DEFAULT &&
+    err.error != null &&
+    err.error is SocketException;
   }
 }
