3896b9fa752c79be039329fe56f640f9528b0055
SherlockHolmes2045 <lemovou@gmail.com>
2021-03-16 13:55:10 +0100

    fixed issue with https

--- lib/di.dart
+++ lib/di.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class Di {
-  final String apiUrl = "http://mapane.smartcodegroup.com";
+  final String apiUrl = "https://mapane.smartcodegroup.com";
 
   Dio dio = new Dio();
 }
