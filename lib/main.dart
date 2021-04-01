d6ff8a2c261baff0e8c5bdaaa4e0ce506c48798b
SherlockHolmes2045 <lemovou@gmail.com>
2021-04-01 13:34:11 +0100

    working on splashscreena animations

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:near_me/routes.dart';
 import 'package:near_me/screens/splashscreen.dart';
 import 'package:near_me/service_locator.dart';
@@ -23,8 +24,11 @@ class _MyAppState extends State<MyApp> {
 
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mapane',
+      title: 'Near Me',
       routes: Routes.routes,
+      theme: ThemeData(
+        textTheme: GoogleFonts.firaSansTextTheme(),
+      ),
       home: SplashScreen(),
       debugShowCheckedModeBanner: false,
     );
--- lib/screens/splashscreen.dart
+++ lib/screens/splashscreen.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:near_me/constants/assets.dart';
+import 'package:near_me/utils/hexcolor.dart';
 import 'package:near_me/utils/size_config.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -12,6 +13,7 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   bool _navigate = false;
+
 
   @override
   void initState() {
@@ -42,31 +44,109 @@ class _SplashScreenState extends State<SplashScreen> {
                     gradient: LinearGradient(
                       begin:  Alignment.topCenter,
                       end: Alignment.bottomCenter,
-                      colors: [Colors.black.withOpacity(0.7),Colors.black,Colors.blue],
+                      colors: [Colors.black.withOpacity(0.7),Colors.black,HexColor("#2082D6")],
                       stops: [0.58,0.8,1.0]
                     )
                   ) : null,
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
+                  child: Stack(
                       children: [
-                        Image.asset(
-                            Assets.logoPng
-                        ),
-                        if(_navigate)...[
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              "Les meilleures compétences à proximité",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20.0,
-                                  fontWeight: FontWeight.bold
-                              ),
+                        AnimatedPadding(
+                          duration: Duration(seconds: 3),
+                          padding: _navigate ? EdgeInsets.only(bottom: SizeConfig.screenHeight/ 4.3) : EdgeInsets.all(0.01),
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: Image.asset(
+                                Assets.logoPng
                             ),
-                          )
+                          ),
+                        ),
+                          AnimatedPadding(
+                            duration: Duration(seconds: 1),
+                            padding: _navigate ? EdgeInsets.only(bottom: SizeConfig.screenHeight / 2.7,left: SizeConfig.blockSizeHorizontal * 2,right: SizeConfig.blockSizeHorizontal * 2) : EdgeInsets.all(0.01),
+                            child: Align(
+                              alignment: Alignment.bottomCenter,
+                              child: _navigate ? Text(
+                                "Les meilleures compétences à proximité",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 19.0,
+                                    fontWeight: FontWeight.bold
+                                ),
+                              ) : SizedBox(),
+                            )
+                          ),
+                        AnimatedPadding(
+                            duration: Duration(seconds: 1),
+                            padding: _navigate ? EdgeInsets.only(bottom: SizeConfig.screenHeight / 4) : EdgeInsets.all(0.01),
+                            child: Align(
+                              alignment: Alignment.bottomCenter,
+                              child: _navigate ? Container(
+                                width: SizeConfig.screenWidth /1.3,
+                                child: Text(
+                                  "Near Me vous permet de trouver des personnes ou services à proximité de vous pour répondre à votre besoin.",
+                                  textAlign: TextAlign.justify,
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ) : SizedBox(),
+                            )
+                          ),
+                        AnimatedPadding(
+                            padding: _navigate ? EdgeInsets.only(bottom: SizeConfig.screenHeight / 40) : EdgeInsets.all(0.01),
+                            duration: Duration(seconds: 1),
+                          child: Align(
+                            alignment: Alignment.bottomCenter,
+                            child: _navigate ?
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Container(
+                                  width: SizeConfig.screenWidth / 1.2,
+                                  height: SizeConfig.screenHeight / 14,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.all(Radius.circular(SizeConfig.blockSizeHorizontal * 6)),
+                                    color: HexColor("#2082D6"),
+                                  ),
+                                  child: ListTile(
+                                    leading: SizedBox(),
+                                      title: Text(
+                                        "Créer mon compte",
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 20
+                                        ),
+                                      ),
+                                      trailing: Icon(
+                                        Icons.arrow_forward_rounded,
+                                        color: Colors.white,
+                                      ),
+                                      onTap: (){}
+                                      ),
+                                ),
+                                SizedBox(
+                                  height: SizeConfig.blockSizeVertical * 2,
+                                ),
+                                Text(
+                                  "En créant mon compte, j'accepte",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                    fontSize: 12.0
+                                  ),
+                                ),
+                                Text(
+                                  "les conditions d'utilisations",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                      fontSize: 12.0
+                                  ),
+                                )
+                              ],
+                            ) : SizedBox()
+                          ),
+                        )
                         ]
-                      ],
                     ),
                   ),
                 ))
