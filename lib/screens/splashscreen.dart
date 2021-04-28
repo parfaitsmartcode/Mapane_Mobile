01d9d7252099241b24e165ca314d7f605d81d00c
parfaitsmartcode <parfait@smartcodegroup.com>
2021-04-28 12:20:23 +0100

    new

--- lib/screens/splashscreen.dart
+++ lib/screens/splashscreen.dart
@@ -14,7 +14,6 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   bool _navigate = false;
 
-
   @override
   void initState() {
     // TODO: implement initState
@@ -33,55 +32,63 @@ class _SplashScreenState extends State<SplashScreen> {
         child: Scaffold(
             backgroundColor: Colors.black,
             body: Container(
-                constraints: BoxConstraints.expand(),
+              constraints: BoxConstraints.expand(),
+              decoration: _navigate
+                  ? BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage(Assets.backgroundImage)))
+                  : null,
+              child: Container(
                 decoration: _navigate
                     ? BoxDecoration(
-                        image: DecorationImage(
-                            image: AssetImage(Assets.backgroundImage)))
+                        gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [Colors.black.withOpacity(0.7),Colors.black,HexColor("#2082D6")],
+                        stops: [0.58,0.8,1.0]
+                      ))
                     : null,
-                child: Container(
-                  decoration: _navigate ? BoxDecoration(
-                    gradient: LinearGradient(
-                      begin:  Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [Colors.black.withOpacity(0.7),Colors.black,HexColor("#2082D6")],
-                      stops: [0.58,0.8,1.0]
-                    )
-                  ) : null,
-                  child: Stack(
-                      children: [
-                        AnimatedPadding(
-                          duration: Duration(seconds: 3),
-                          padding: _navigate ? EdgeInsets.only(bottom: SizeConfig.screenHeight/ 4.3) : EdgeInsets.all(0.01),
-                          child: Align(
-                            alignment: Alignment.center,
-                            child: Image.asset(
-                                Assets.logoPng
-                            ),
-                          ),
-                        ),
-                          AnimatedPadding(
-                            duration: Duration(seconds: 1),
-                            padding: _navigate ? EdgeInsets.only(bottom: SizeConfig.screenHeight / 2.7,left: SizeConfig.blockSizeHorizontal * 2,right: SizeConfig.blockSizeHorizontal * 2) : EdgeInsets.all(0.01),
-                            child: Align(
-                              alignment: Alignment.bottomCenter,
-                              child: _navigate ? Text(
+                child: Stack(children: [
+                  AnimatedPadding(
+                    duration: Duration(seconds: 3),
+                    padding: _navigate
+                        ? EdgeInsets.only(bottom: SizeConfig.screenHeight / 4.3)
+                        : EdgeInsets.all(0.01),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Image.asset(Assets.logoPng),
+                    ),
+                  ),
+                  AnimatedPadding(
+                      duration: Duration(seconds: 1),
+                      padding: _navigate
+                          ? EdgeInsets.only(
+                              bottom: SizeConfig.screenHeight / 2.7,
+                              left: SizeConfig.blockSizeHorizontal * 2,
+                              right: SizeConfig.blockSizeHorizontal * 2)
+                          : EdgeInsets.all(0.01),
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: _navigate
+                            ? Text(
                                 "Les meilleures compétences à proximité",
                                 style: TextStyle(
                                     color: Colors.white,
                                     fontSize: 19.0,
-                                    fontWeight: FontWeight.bold
-                                ),
-                              ) : SizedBox(),
-                            )
-                          ),
-                        AnimatedPadding(
-                            duration: Duration(seconds: 1),
-                            padding: _navigate ? EdgeInsets.only(bottom: SizeConfig.screenHeight / 4) : EdgeInsets.all(0.01),
-                            child: Align(
-                              alignment: Alignment.bottomCenter,
-                              child: _navigate ? Container(
-                                width: SizeConfig.screenWidth /1.3,
+                                    fontWeight: FontWeight.bold),
+                              )
+                            : SizedBox(),
+                      )),
+                  AnimatedPadding(
+                      duration: Duration(seconds: 1),
+                      padding: _navigate
+                          ? EdgeInsets.only(bottom: SizeConfig.screenHeight / 4)
+                          : EdgeInsets.all(0.01),
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: _navigate
+                            ? Container(
+                                width: SizeConfig.screenWidth / 1.3,
                                 child: Text(
                                   "Near Me vous permet de trouver des personnes ou services à proximité de vous pour répondre à votre besoin.",
                                   textAlign: TextAlign.justify,
@@ -90,66 +97,63 @@ class _SplashScreenState extends State<SplashScreen> {
                                     fontSize: 18,
                                   ),
                                 ),
-                              ) : SizedBox(),
-                            )
-                          ),
-                        AnimatedPadding(
-                            padding: _navigate ? EdgeInsets.only(bottom: SizeConfig.screenHeight / 40) : EdgeInsets.all(0.01),
-                            duration: Duration(seconds: 1),
-                          child: Align(
-                            alignment: Alignment.bottomCenter,
-                            child: _navigate ?
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Container(
-                                  width: SizeConfig.screenWidth / 1.2,
-                                  height: SizeConfig.screenHeight / 14,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.all(Radius.circular(SizeConfig.blockSizeHorizontal * 6)),
-                                    color: HexColor("#2082D6"),
-                                  ),
-                                  child: ListTile(
-                                    leading: SizedBox(),
-                                      title: Text(
-                                        "Créer mon compte",
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 20
+                              )
+                            : SizedBox(),
+                      )),
+                  AnimatedPadding(
+                    padding: _navigate
+                        ? EdgeInsets.only(bottom: SizeConfig.screenHeight / 40)
+                        : EdgeInsets.all(0.01),
+                    duration: Duration(seconds: 1),
+                    child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: _navigate
+                            ? Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Container(
+                                    width: SizeConfig.screenWidth / 1.2,
+                                    height: SizeConfig.screenHeight / 14,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(
+                                              SizeConfig.blockSizeHorizontal *
+                                                  6)),
+                                      color: HexColor("#2082D6"),
+                                    ),
+                                    child: ListTile(
+                                        leading: SizedBox(),
+                                        title: Text(
+                                          "Créer mon compte",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 20),
                                         ),
-                                      ),
-                                      trailing: Icon(
-                                        Icons.arrow_forward_rounded,
-                                        color: Colors.white,
-                                      ),
-                                      onTap: (){}
-                                      ),
-                                ),
-                                SizedBox(
-                                  height: SizeConfig.blockSizeVertical * 2,
-                                ),
-                                Text(
-                                  "En créant mon compte, j'accepte",
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                    fontSize: 12.0
+                                        trailing: Icon(
+                                          Icons.arrow_forward_rounded,
+                                          color: Colors.white,
+                                        ),
+                                        onTap: () {}),
                                   ),
-                                ),
-                                Text(
-                                  "les conditions d'utilisations",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                      fontSize: 12.0
+                                  SizedBox(
+                                    height: SizeConfig.blockSizeVertical * 2,
                                   ),
-                                )
-                              ],
-                            ) : SizedBox()
-                          ),
-                        )
-                        ]
-                    ),
-                  ),
-                ))
-            );
+                                  Text(
+                                    "En créant mon compte, j'accepte",
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 12.0),
+                                  ),
+                                  Text(
+                                    "les conditions d'utilisations",
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 12.0),
+                                  )
+                                ],
+                              )
+                            : SizedBox()),
+                  )
+                ]),
+              ),
+            )));
   }
 }
